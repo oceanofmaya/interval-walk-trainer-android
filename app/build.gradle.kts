4fb1c28040ee3d7c8468ae0d325c5456290e24d7
oceanofmaya <drkishoremurthy@gmail.com>
2026-01-17 21:30:19 -0500

    chore: increment versionCode to 2 for beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.oceanofmaya.intervalwalktrainer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0-beta.2"
     }
 
