0f8edd8949dc214b89436caa02f79c392d1e402e
oceanofmaya <drkishoremurthy@gmail.com>
2026-01-27 22:15:50 -0500

    fix: release 1.0.0-beta.18
    Unify settings headings and add Privacy & Terms heading.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.oceanofmaya.intervalwalktrainer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.0.0-beta.17"
+        versionCode = 18
+        versionName = "1.0.0-beta.18"
     }
 
     signingConfigs {
